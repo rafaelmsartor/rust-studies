8e0f57a5b75f371f8b7579302f6203b50c4361fc
Rafael Meneguzzo Sartor <rafaelmsartor@gmail.com>
2019-08-11 13:43:12 -0300

    formatted using cargo fmt

--- rust_programming_by_example/tetris/src/main.rs
+++ rust_programming_by_example/tetris/src/main.rs
@@ -1,40 +1,46 @@
 extern crate sdl2;
 
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 
-use std::time::Duration;
 use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let sdl_context = sdl2::init().expect("SLD initialization failed");
-    let video_subsystem = sdl_context.video().expect("Couldn't get SDL video subsystem");
+    let video_subsystem = sdl_context
+        .video()
+        .expect("Couldn't get SDL video subsystem");
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
-                                .position_centered()
-                                .opengl()
-                                .build()
-                                .expect("Failed to create window");
-    
-    let mut canvas = window.into_canvas()
-                           .build()
-                           .expect("Failed to convert window into canvas");
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .expect("Failed to create window");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Failed to convert window into canvas");
 
     canvas.set_draw_color(Color::RGB(0, 0, 255));
     canvas.clear();
     canvas.present();
 
-    let mut event_pump = sdl_context.event_pump()
-                                    .expect("Failed to get SDL event pump");
-    
+    let mut event_pump = sdl_context
+        .event_pump()
+        .expect("Failed to get SDL event pump");
+
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             }
         }
