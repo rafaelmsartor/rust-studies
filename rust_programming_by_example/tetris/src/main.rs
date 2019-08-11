a0482ecc52f84ae5c56519494233ea998a1e84c6
Rafael Meneguzzo Sartor <rafaelmsartor@gmail.com>
2019-08-11 16:56:17 -0300

    Added functions to save and load the highscores to a file

--- rust_programming_by_example/tetris/src/main.rs
+++ rust_programming_by_example/tetris/src/main.rs
@@ -1,46 +1,111 @@
 extern crate sdl2;
 
 use sdl2::event::Event;
+use sdl2::image::{LoadTexture, INIT_JPG, INIT_PNG};
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::render::TextureCreator;
-use sdl2::image::{LoadTexture, INIT_PNG, INIT_JPG};
 
 use std::time::Duration;
 
+use std::fs::File;
+use std::io::{self, Read, Write};
+
+fn write_to_file(contents: String, file_name: &str) -> io::Result<()> {
+    let mut f = File::create(file_name)?;
+    f.write_all(contents.as_bytes())
+}
+
+fn read_from_file(file_name: &str) -> io::Result<String> {
+    let mut f = File::open(file_name)?;
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn slice_to_string(slice: &[u32]) -> String {
+    slice
+        .iter()
+        .map(|highscore| highscore.to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn line_to_slice(line: &str) -> Vec<u32> {
+    line.split(" ")
+        .filter_map(|nb| nb.parse::<u32>().ok())
+        .collect()
+}
+
+fn save_highscores_and_lines(highscores: &[u32], number_of_lines: &[u32]) -> bool {
+    let s_highscores = slice_to_string(highscores);
+    let s_number_of_lines = slice_to_string(number_of_lines);
+
+    write_to_file(format!("{}\n{}\n", s_highscores, s_number_of_lines), "scores.txt").is_ok()
+}
+
+fn load_highscores_and_lines() -> Option<(Vec<u32>, Vec<u32>)> {
+    if let Ok(content) = read_from_file("scores.txt") {
+        let mut lines = content.splitn(2, "\n")
+                               .map(|line| line_to_slice(line))
+                               .collect::<Vec<_>>();
+        if lines.len() == 2 {
+            let (number_lines, highscores) = (lines.pop().unwrap(), lines.pop().unwrap());
+            Some((highscores, number_lines))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
 fn main() {
     let sdl_context = sdl2::init().expect("SDL initialization failed");
-    let video_subsystem = sdl_context.video().expect("Couldn't get SDL video subsystem");
+    let video_subsystem = sdl_context
+        .video()
+        .expect("Couldn't get SDL video subsystem");
 
     sdl2::image::init(INIT_PNG | INIT_JPG).expect("Couldn't initialize image context");
 
-    let window = video_subsystem.window("rust-sdl2 image demo", 800, 600)
+    let window = video_subsystem
+        .window("rust-sdl2 image demo", 800, 600)
         .position_centered()
         .opengl()
         .build()
         .expect("Failed to create window");
 
-    let mut canvas = window.into_canvas().build().expect("Failed to convert window into canvas");
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Failed to convert window into canvas");
 
     let texture_creator: TextureCreator<_> = canvas.texture_creator();
-    let image_texture = texture_creator.load_texture("assets/my_image.jpg").expect("Couldn't load image");
+    let image_texture = texture_creator
+        .load_texture("assets/my_image.jpg")
+        .expect("Couldn't load image");
 
-    let mut event_pump = sdl_context.event_pump().expect("Failed to get SDL event pump");
+    let mut event_pump = sdl_context
+        .event_pump()
+        .expect("Failed to get SDL event pump");
 
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown{ keycode: Some(Keycode::Escape), ..} => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             };
         }
 
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-        canvas.copy(&image_texture, None, None).expect("Render failed");
+        canvas
+            .copy(&image_texture, None, None)
+            .expect("Render failed");
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
