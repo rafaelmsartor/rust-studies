373146d70327c3fac8f6ddce364780c912644f2f
Rafael Meneguzzo Sartor <rafaelmsartor@gmail.com>
2019-08-17 19:58:19 -0300

    Added command line argument and config file reading

--- microservices_in_rust/ch03_logging_and_configuration/src/main.rs
+++ microservices_in_rust/ch03_logging_and_configuration/src/main.rs
@@ -1,23 +1,74 @@
 use hyper::{Body, Response, Server};
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
-use log::{debug, info, trace};
+use log::{debug, info, warn, trace};
 use dotenv::dotenv;
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, App};
+use serde_derive::Deserialize;
 
 use std::env;
+use std::io::{self, Read};
+use std::fs::File;
+use std::net::SocketAddr;
 
+#[derive(Deserialize)]
+struct Config {
+    address: SocketAddr,
+}
 
 fn main() {
     dotenv().ok();
+
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("address")
+            .short("a")
+            .long("address")
+            .value_name("ADDRESS")
+            .help("Sets an address")
+            .takes_value(true))
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .value_name("FILE")
+            .help("Sets a custom config file")
+            .takes_value(true))
+        .get_matches();
 
     pretty_env_logger::init();
     info!("Rand microservice - 0.1.0");
     trace!("Starting...");
 
-    let addr = env::var("ADDRESS")
-        .unwrap_or_else(|_|"127.0.0.1:8080".into())
-        .parse()
-        .expect("Can't parse ADDRESS variable");
+    let config_file_name = matches.value_of("config")
+        .map(|s| s.to_owned())
+        .or_else(|| Some(String::from("microservice.toml")))
+        .unwrap();
+
+    let config = File::open(config_file_name)
+        .and_then(|mut file| {
+            let mut buffer = String::new();
+            file.read_to_string(&mut buffer)?;
+            Ok(buffer)
+        })
+        .and_then(|buffer| {
+            toml::from_str::<Config>(&buffer)
+                .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+        })
+        .map_err(|err| {
+            warn!("Can't read config file: {}", err);
+        })
+        .ok();
+
+
+    let addr = matches.value_of("address")
+        .map(|s| s.to_owned())
+        .or(env::var("ADDRESS").ok())
+        .and_then(|addr| addr.parse().ok())
+        .or(config.map(|config| config.address))
+        .or_else(|| Some(([127, 0, 0, 1], 8080).into()))
+        .unwrap();
 
     debug!("Trying to bind to server address {}", addr);
     let builder = Server::bind(&addr);
